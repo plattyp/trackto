00b8f1440c93a2f69dd600f52f811664c786f190
Andrew Platkin <andrew.platkin@gmail.com>
2015-04-14 21:02:56 -0500

    fixed a spelling error

--- app/controllers/progresses_controller.rb
+++ app/controllers/progresses_controller.rb
@@ -33,7 +33,7 @@ class ProgressesController < ApplicationController
         format.json { render json: @progress.to_json, status: 200 }
       else
         format.html { render :new }
-        format.json { render 'layout/json/errors.json.erb', status: :unprocessable_entity }
+        format.json { render 'layouts/json/errors.json.erb', status: :unprocessable_entity }
       end
     end
   end
