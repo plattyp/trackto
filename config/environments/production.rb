27599107ccd1fc63e1667a8e849173bd51778687
Andrew Platkin <andrew.platkin@gmail.com>
2015-08-09 19:47:20 -0500

    trying to ignore in environment file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,6 +32,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.assets.precompile = [ Proc.new{ |path| !File.extname(path).in?(['.js', '.css', '.less', 'js.min.gzip']) }, /application.(css|js)$/ ]
+
   # Generate digests for assets URLs.
   config.assets.digest = true
 
