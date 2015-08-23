77d2ec813c8d3c435e06a720c8e06ca96d53153d
Andrew Platkin <andrew.platkin@gmail.com>
2015-08-23 16:50:29 -0500

    updated mailing settings for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,12 +14,25 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.action_mailer.default_url_options = { host: 'trackto.co', port: 80 }
+  config.action_mailer.default_url_options = { host: 'http://www.trackto.co', port: 80 }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
+
+  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = { :address => 'http://trackto.co', :port => 1025 }
+
+  ActionMailer::Base.smtp_settings = {
+    :user_name => Rails.application.secrets.email_username,
+    :password => Rails.application.secrets.email_password,
+    :domain => 'trackto.co',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
