984831f35e8cb9de927f32e405e2373c623b067c
Andrew Platkin <andrew.platkin@gmail.com>
2015-08-23 15:53:19 -0500

    updated app js file

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -10,41 +10,41 @@
     // For authentication
     .config(function($authProvider) {
       $authProvider.configure({
-        apiUrl:                  '/api',
-        tokenValidationPath:     '/auth/validate_token',
-        signOutUrl:              '/auth/sign_out',
-        emailRegistrationPath:   '/auth',
-        accountUpdatePath:       '/auth',
-        accountDeletePath:       '/auth',
-        confirmationSuccessUrl:  window.location.href,
-        passwordResetPath:       '/auth/password',
-        passwordUpdatePath:      '/auth/password',
-        passwordResetSuccessUrl: window.location.href,
-        emailSignInPath:         '/auth/sign_in',
-        storage:                 'cookies',
-        forceValidateToken:      false,
-        proxyIf:                 function() { return false; },
-        proxyUrl:                '/proxy',
-        tokenFormat: {
-          "access-token": "{{ token }}",
-          "token-type":   "Bearer",
-          "client":       "{{ clientId }}",
-          "expiry":       "{{ expiry }}",
-          "uid":          "{{ uid }}"
-        },
-        parseExpiry: function(headers) {
-          // convert from UTC ruby (seconds) to UTC js (milliseconds)
-          return (parseInt(headers.expiry) * 1000) || null;
-        },
-        handleLoginResponse: function(response) {
-          return response.data;
-        },
-        handleAccountUpdateResponse: function(response) {
-          return response.data;
-        },
-        handleTokenValidationResponse: function(response) {
-          return response.data;
-        }
+      apiUrl:'/api',
+      tokenValidationPath:'/auth/validate_token',
+      signOutUrl:'/auth/sign_out',
+      emailRegistrationPath:'/auth',
+      accountUpdatePath:'/auth',
+      accountDeletePath:'/auth',
+      confirmationSuccessUrl:window.location.href,
+      passwordResetPath:'/auth/password',
+      passwordUpdatePath:'/auth/password',
+      passwordResetSuccessUrl:window.location.href,
+      emailSignInPath:'/auth/sign_in',
+      storage:'cookies',
+      forceValidateToken:false,
+      proxyIf:function() { return false; },
+      proxyUrl:'/proxy',
+      tokenFormat: {
+        "access-token": "{{ token }}",
+        "token-type":"Bearer",
+        "client":"{{ clientId }}",
+        "expiry":"{{ expiry }}",
+        "uid":"{{ uid }}"
+      },
+      parseExpiry: function(headers) {
+        // convert from UTC ruby (seconds) to UTC js (milliseconds)
+        return (parseInt(headers.expiry) * 1000) || null;
+      },
+      handleLoginResponse: function(response) {
+        return response.data;
+      },
+      handleAccountUpdateResponse: function(response) {
+        return response.data;
+      },
+      handleTokenValidationResponse: function(response) {
+        return response.data;
+      }
       });
     });
 })();
